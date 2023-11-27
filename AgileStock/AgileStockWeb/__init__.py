886e48149bfcc29b3e92c56e7b77b9fde2484967
Robert A Howell <rhowell91@live.com>
2023-11-27 00:23:16 -0600

    mysql table create

--- AgileStock/AgileStockWeb/__init__.py
+++ AgileStock/AgileStockWeb/__init__.py
@@ -17,8 +17,64 @@ The flask application package.
 #    Press CTRL+C to quit
 
 
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 import AgileStockWeb.views
+
+import os
+import pymysql
+# #SQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '4LocalDB'
+app.config['MYSQL_DB'] = 'agilestockinv'
+
+mysql = pymysql.connect(
+    host = app.config['MYSQL_HOST'],
+    user = app.config['MYSQL_USER'],
+    password= app.config['MYSQL_PASSWORD'],
+    db = app.config['MYSQL_DB']
+)
+
+
+def create_table():
+    try:
+        print('Creating Table Started =====')
+        cur = mysql.cursor()
+        cur.execute(
+            '''
+            CREATE TABLE IF NOT EXISTS items (
+                id INT AUTO_INCREMENT PRIMARY KEY ,
+                name VARCHAR(255) NOT NULL,
+                description TEXT
+            )
+            '''
+        )
+        mysql.commit()
+        cur.close()
+        print('Items Table Created =====')
+    except Exception as e:
+        print("Error while creating table",e)
+
+create_table()
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True)
+#     email = db.Column(db.String(120), unique=True)
+
+#     def __init__(self, username, email):
+#         self.username = username
+#         self.email = email
+
+#     def __repr__(self):
+#         return '<User %r>' % self.username
+
+
+# #an inventory object
+# #keyBoard = Inventory.Inventory("Keyboard", 0)
+# #print(f"name:{keyBoard.name:s}, itemNum:{keyBoard.itemNum}")
+# #an inventory object end
+
