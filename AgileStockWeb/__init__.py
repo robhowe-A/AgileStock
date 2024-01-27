387f017f061c186e64045d4fb0d87a2fe5962b04
addivaton <106926480+addivaton@users.noreply.github.com>
2024-01-27 16:40:01 -0600

    Added edit functions for the database

--- AgileStockWeb/__init__.py
+++ AgileStockWeb/__init__.py
@@ -1,49 +1,50 @@
 """
 The flask application package.
 """
+
 import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-#from AgileStockWeb.models.out_formatter import logger
+# from AgileStockWeb.models.out_formatter import logger
 from AgileStockWeb.database.db import CreateDatabase
+
 if not os.environ.get("AZURE_ENVIRONMENT") == "AZUREPROD":
     # These (commented) credentials were used for local MySQL database connection string
     # #app.config is a hash variable needed for a connection string
-    app.config['MYSQL_HOST'] = '127.0.0.1'
-    app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = '4LocalDB'
-    app.config['MYSQL_DB'] = 'agilestockinv'
+    app.config["MYSQL_HOST"] = "127.0.0.1"
+    app.config["MYSQL_USER"] = "root"
+    app.config["MYSQL_PASSWORD"] = "mylocalpassword"
+    app.config["MYSQL_DB"] = "agilestockinv"
 else:
-    #The below (4) app config variables are used only in azure deployed environment
-    app.config['MYSQL_HOST'] = os.environ.get("AZURE_MYSQL_HOST")
-    app.config['MYSQL_USER'] = os.environ.get("AZURE_MYSQL_USER")
-    app.config['MYSQL_PASSWORD'] = os.environ.get("AZURE_MYSQL_PASSWORD")
-    app.config['MYSQL_DB'] = os.environ.get("AZURE_MYSQL_NAME")
+    # The below (4) app config variables are used only in azure deployed environment
+    app.config["MYSQL_HOST"] = os.environ.get("AZURE_MYSQL_HOST")
+    app.config["MYSQL_USER"] = os.environ.get("AZURE_MYSQL_USER")
+    app.config["MYSQL_PASSWORD"] = os.environ.get("AZURE_MYSQL_PASSWORD")
+    app.config["MYSQL_DB"] = os.environ.get("AZURE_MYSQL_NAME")
 
 db = CreateDatabase(app)
 
-
 from AgileStockWeb.models.book import AS_BOOK
 
-wolfBook = AS_BOOK(
-    "The Wolves of Winter",
-    "Tyrell Johnson",
-    "Scribner",
-    "2018",
-    "Action",
-    "9781501155680"
+APIexampleitem_InventoryFromScannerApp = AS_BOOK(
+    "TheActualBook",
+    "Books author",
+    "action books",
+    "5432112345ABCD",
+    "Comedy",
+    "1234567890",
 )
-
-APIexampleitem_InventoryFromScannerApp = AS_BOOK("TheActualBook", "Books author", "action books", "5432112345ABCD", "Comedy", "1234567890")
-db.insert_intoAS_BOOK(APIexampleitem_InventoryFromScannerApp.isbn,
+db.insert_intoAS_BOOK(
+    APIexampleitem_InventoryFromScannerApp.isbn,
     APIexampleitem_InventoryFromScannerApp.title,
     APIexampleitem_InventoryFromScannerApp.author,
     APIexampleitem_InventoryFromScannerApp.publisher,
-    APIexampleitem_InventoryFromScannerApp.publishedDate, 
-    APIexampleitem_InventoryFromScannerApp.genre)
+    APIexampleitem_InventoryFromScannerApp.publishedDate,
+    APIexampleitem_InventoryFromScannerApp.genre,
+)
 
 
 from AgileStockWeb import views
--- AgileStockWeb/models/out_formatter.py
+++ AgileStockWeb/models/out_formatter.py
@@ -1,5 +1,6 @@
 import logging
 
+
 class CustomFormatter(logging.Formatter):
 
     grey = "\x1b[38;20m"
@@ -7,21 +8,24 @@ class CustomFormatter(logging.Formatter):
     red = "\x1b[31;20m"
     bold_red = "\x1b[31;1m"
     reset = "\x1b[0m"
-    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    format = (
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    )
 
     FORMATS = {
         logging.DEBUG: grey + format + reset,
         logging.INFO: grey + format + reset,
         logging.WARNING: yellow + format + reset,
         logging.ERROR: red + format + reset,
-        logging.CRITICAL: bold_red + format + reset
+        logging.CRITICAL: bold_red + format + reset,
     }
 
     def format(self, record):
         log_fmt = self.FORMATS.get(record.levelno)
         formatter = logging.Formatter(log_fmt)
         return formatter.format(record)
-    
+
+
 logger = logging.getLogger("AGILESTOCKWEB")
 logger.setLevel(logging.INFO)
 ch = logging.StreamHandler()
