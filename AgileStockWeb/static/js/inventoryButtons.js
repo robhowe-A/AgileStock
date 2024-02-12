f35716cafbfd21919cbf25b5e2e22aacfb03842f
Robert A Howell <rhowell91@live.com>
2024-02-11 19:10:21 -0600

    html validator fixes

--- AgileStockWeb/static/js/inventoryButtons.js
+++ AgileStockWeb/static/js/inventoryButtons.js
@@ -3,17 +3,19 @@ const inventoryButtons = {
     //Inventory item buttons function for edit & delete
     const dialog = document.querySelectorAll("dialog.deleteConfirm");
     const showButton = document.getElementsByClassName("DelButton");
-    const closeButton = document.querySelectorAll("dialog button[autofocus]");
+    const closeButton = document.querySelectorAll("dialog .cancelAutofocus");
 
     // "Show the dialog" button opens the dialog modally
     for (let i = 0; i < showButton.length; i++) {
       showButton[i].addEventListener("click", () => {
+        closeButton[i].toggleAttribute("autofocus");
         dialog[i].showModal();
       });
 
       // "Close" button closes the dialog
       closeButton[i].addEventListener("click", () => {
         dialog[i].close();
+        closeButton[i].toggleAttribute("autofocus");
       });
     }
   },
